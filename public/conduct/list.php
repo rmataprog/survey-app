9d9a82aa3f3d333eccbd1492d5278f91a5e65ac2
rubenatsaporous <rmata.ufs@gmail.com>
2023-05-21 22:10:50 -0400

    improved pagination

--- public/conduct/list.php
+++ public/conduct/list.php
@@ -8,7 +8,7 @@ if(!$cms->getSession()->logged_in) {
 
 $user_id = $_SESSION['id'];
 $is_coordinator = $_SESSION['coordinator'];
-$show = filter_input(INPUT_GET, 'show', FILTER_VALIDATE_INT) ? intval(filter_input(INPUT_GET, 'show')) : 5;
+$show = filter_input(INPUT_GET, 'show', FILTER_VALIDATE_INT) ? intval(filter_input(INPUT_GET, 'show')) : 3;
 $offset = filter_input(INPUT_GET, 'offset', FILTER_VALIDATE_INT) ? intval(filter_input(INPUT_GET, 'offset')) : 0;
 
 if($is_coordinator) {
@@ -16,7 +16,7 @@ if($is_coordinator) {
     $surveys = $cms->getSurvey()->get_surveys_for_id($user_id, $show, $offset);
     $data["surveys"] = $surveys;
     $data["total"] = $surveys_count;
-    $data["current"] = floor($offset / 5);
+    $data["current"] = floor($offset / 3);
     echo $twig->render("conduct/list.html", $data);
 } else {
     redirect(DOC_ROOT . 'view/view.php');
